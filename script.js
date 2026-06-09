648823fc0a6439f51464374eccb5f52ed40a7b0e
parthpotenza <parth.patel@potenzaglobalsolutions.com>
2026-06-09 23:29:06 +0530

    Starting to add JavaScript Functionallity

---
+++ script.js
@@ -0,0 +1,8 @@
+// Element Selection
+const submit = document.querySelector('#submitBtn');
+const reset = document.querySelector('#resetBtn');
+const form = document.querySelector('#srsForm');
+const tableBody = document.querySelector('#tableBody');
+const tableContainer = document.getElementById('#tableContainer');
+const emptyState = document.getElementById('emptyState');
+const recordCount = document.getElementById('record');
